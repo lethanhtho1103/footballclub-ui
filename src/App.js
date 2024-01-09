28295c160b81eb9839700974d825d41d0acfac3a
lethanhtho1103 <lethanhtho1953@gamil.com>
2024-01-09 22:00:44 +0700

    Cài đặt môi trường

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { Fragment } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let LayOut = Fragment;
+            if (route.layout) {
+              LayOut = route.layout;
+            }
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <LayOut>
+                    <Page />
+                  </LayOut>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import Home from '~/pages/Home';
+import Login from '~/pages/Login';
+import Register from '~/pages/Register';
+
+const publicRoutes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    component: Register,
+  },
+];
+
+export { publicRoutes };
